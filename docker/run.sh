96710d285736f1c1ef364aaec3f130d80abae9e6
Skylar Wurster <swwurster@gmail.com>
2025-06-22 13:52:03 -0700

    finally working

--- docker/run.sh
+++ docker/run.sh
@@ -7,6 +7,7 @@ PROJECT_DIR=$(pwd)
 echo "Running Docker image: $IMAGE_TAG with GPU support and mounted volume"
 docker run --rm -it \
   --gpus all \
+  --shm-size=1g \
   -v "$PROJECT_DIR":/dynamic-gsplats \
   -w /dynamic-gsplats \
-  "$IMAGE_TAG" 
+  "$IMAGE_TAG"
